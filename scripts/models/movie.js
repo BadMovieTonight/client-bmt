364dbd4d542e87f7ba290475332b154077517d91
Benjamin Taylor <staylor.ben@gmail.com>
2018-05-25 14:32:52 -0700

    added files

---
+++ scripts/models/movie.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var app = app || {};
+
+(function(module) {
+
+  function Movie(movieObject){
+    Object.keys(movieObject).forEach(key => this[key] = bookObject[key]);
+  }
+
+  Movie.all = [];
+
+})(app)
