231e8bce679d2ab2c9276893306001aa765c2603
Tracy Williams <selpilot@gmail.com>
2018-05-29 10:08:03 -0700

    search bar returning and rendering movies

--- scripts/views/movieView.js
+++ scripts/views/movieView.js
@@ -14,5 +14,25 @@ var app = app || {};
     app.Movie.all.forEach(elem => {$movieList.append(elem.toHtml());});
   };
 
+  movieView.handleGeneralSearch = function(ctx) {
+    console.log(ctx);
+    console.log($('#search').val());
+    $.get(`${app.ENVIRONMENT.apiUrl}/bmt/search`,
+    {searchFor: $('#search').val()})
+      .then(response => {
+        // console.log(response.results);
+        // response.results.forEach(resObj => {
+        //   console.log(resObj.media_type, resObj.title);
+        // });
+        app.Movie.all = response.results
+          .filter(o => o.media_type === 'movie')  // an array of movies
+          .map(o => new app.Movie(o));
+        console.log(app.Movie.all);
+        movieView.initIndexPage();
+        // callback();
+      })
+      .catch(err => console.log('that didn\'t work'));
+  }
+
   module.movieView = movieView;
 })(app);
