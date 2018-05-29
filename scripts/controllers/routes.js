1825254d4cd0da8beb73a5b08a8d06abe61c0a56
paulritzman <ritzman.paul@gmail.com>
2018-05-29 15:02:55 -0700

    Added developer bios; created directory for bio images

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -6,13 +6,17 @@ page('/', () => {
   app.Movie.fetchAll(app.movieView.initIndexPage);
 });
 
+page('/about', () => {
+  app.showOnly('#about-us');
+});
+
 page('/client-bmt', () => page('/'));
+
+page('/login', () => app.showOnly('#login'));
 
 page('/menu', ctx => {
   app.toggleMenu();
 });
-
-page('/login', () => app.showOnly('#login'));
 
 page('/search', (ctx) => app.movieView.handleGeneralSearch(ctx));
 
