bff7d84c00f614f2d3031878cf01a501812c61f6
paulritzman <ritzman.paul@gmail.com>
2018-05-31 15:09:42 -0700

    Updated developer details for Paul

--- scripts/views/aboutView.js
+++ scripts/views/aboutView.js
@@ -14,7 +14,7 @@ var app = app || {};
 
   Developer.all = [];
 
-  new Developer('Paul Ritzman', 'Paul has a background in Information Security and Computer Forensics. Seeking the opportunity to create tomorrow\'s technology, he opted to pursue development and continues to broaden his skills daily.', '* still thinking', 'bio-imgs/paul.jpg');
+  new Developer('Paul Ritzman', 'Paul has a background in Information Security and Computer Forensics. Seeking the opportunity to create tomorrow\'s technology, he opted to pursue development and continues to broaden his skills daily. In June, Paul will be starting the ASP.NET Core track with the intent of working on large-scale software solutions in the near future.', 'Willy Wonka & the Chocolate Factory', 'bio-imgs/paul.jpg');
 
   new Developer('Tracy Williams', 'Tracy Williams comes to Code Fellows from a long history of jobs both in and out of tech. Tracy graduated from the University of Washington in 1981 with a bachelor\'s in Computer Science with a focus on data structures. Since then he\'s worked as a software engineer, field applications engineer, and sales rep, and has filled various management roles up to VP Engineering &amp; Quality. When he\'s not coding, Tracy enjoys spending time with his wife, Kim, riding one of his two motorcycles, or hanging out at his cabin in the Gifford Pinchot National Forest.', 'Battlefield Earth', 'bio-imgs/tracy.jpg');
 
