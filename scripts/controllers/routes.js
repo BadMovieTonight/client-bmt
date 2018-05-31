7c84c1084a205ecfb4e8a87cea2d565a19f8ff40
paulritzman <ritzman.paul@gmail.com>
2018-05-31 11:08:50 -0700

    Clicking on 'cast and credits' now takes you to top of page; Moved logic for this isto own function in userView and made other page() calls also use this function

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -7,7 +7,7 @@ page('/', () => {
   app.showOnly('#movie-list');
   $('#search').val('');
   app.Movie.fetchAll(app.movieView.initIndexPage);
-  $(document).scrollTop( $('#header').offset().top );
+  app.userView.scrollToTop();
 });
 
 page('/client-bmt', () => page('/'));
@@ -63,14 +63,16 @@ page('/favorites', () => {
 page('/search', (ctx) => app.movieView.handleGeneralSearch(ctx));
 
 page('/search/:page', (ctx) => {
-  $(document).scrollTop($('#header').offset().top);
+  app.userView.scrollToTop();
   app.movieView.handleGeneralSearch(ctx);
 });
 
 page('/movies/:actor', (ctx) => app.movieView.viewBadFilmography(ctx.params.actor));
 
-page('/credits/:movieId', (ctx) => app.movieView.viewCredits(ctx.params.movieId));
-//console.log('credits for movie', ctx.params.movieId));
+page('/credits/:movieId', (ctx) => {
+  app.movieView.viewCredits(ctx.params.movieId);
+  app.userView.scrollToTop();
+});
 
 page('/bmt/person/:id', (ctx) => app.movieView.getPersonDetail(ctx));
 
